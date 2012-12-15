efdf087d02c60c556f0c582bf94ca0b16994d8fb
Nick Niemeir <nick.niemeir@gmail.com>
2012-12-15 14:52:15 -0700

    4 divs

---
+++ public/client.js
@@ -0,0 +1,10 @@
+var f = Flates()
+
+jQuery(function ($) {
+  $('body').html(
+    f.div({ id: 'thumbnail'}) +
+    f.div({ id: 'message'}) +
+    f.div({ id: 'main'}) +
+    f.div({ id: 'bottom'})
+  )
+})
