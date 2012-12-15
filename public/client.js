948ed6a841600beb5d0a0c1c60ef814762202c81
Adam Draper <adamwdraper@gmail.com>
2012-12-15 15:50:25 -0700

    update cameral

--- public/client.js
+++ public/client.js
@@ -1,4 +1,24 @@
-var f = Flates()
+var f = Flates();
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia;
+window.URL = window.URL || window.webkitURL;
+
+var video;
+
+function startCamera() {
+  navigator.getUserMedia({
+    video: true
+  }, function(localMediaStream) {
+    video = document.getElementById("myVideo");
+    video.src = window.URL.createObjectURL(localMediaStream);
+  }, function(error) {
+    console.log(error);
+  });
+
+}
+
+
 
 jQuery(function ($) {
   $('body').html(
@@ -6,5 +26,24 @@ jQuery(function ($) {
     f.div({ id: 'message'}) +
     f.div({ id: 'main'}) +
     f.div({ id: 'bottom'})
-  )
-})
+  );
+
+  video = $('#myVideo');
+
+  $('#main').append('<img id="background">').append('<video id="myVideo" autoplay></video>').append('<button id="capture">Capture</button>');
+
+
+  startCamera();
+
+  $('#thumbnail img').on('click', function (e) {
+    updateTheme($(e.currentTarget));
+  });
+
+  $('#capture').on('click', function (e) {
+    generateCard();
+  });
+
+
+
+
+});
