f130971b9d70fd33f719dba39cc1e52beb67639e
Edward Gilmore <edgilmore@live.com>
2012-12-15 15:26:05 -0700

    added div wrapper & css file

--- public/client.js
+++ public/client.js
@@ -2,6 +2,7 @@ var f = Flates()
 
 jQuery(function ($) {
   $('body').html(
+    f.div({ id: 'wrapper'}) +
     f.div({ id: 'thumbnail'}) +
     f.div({ id: 'message'}) +
     f.div({ id: 'main'}) +
